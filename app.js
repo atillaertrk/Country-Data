c5d91bbe093af0c1776d6f5678ca5a3bb7b7e3ca
atillaertrk <erturk.aa@gmail.com>
2022-09-02 11:50:17 +0300

    Promise Object added

--- app.js
+++ app.js
@@ -7,40 +7,31 @@ const countryInfo1 = document.querySelector(".country1");
 const countryInfo2 = document.querySelector(".country2");
 let contentCount = 0;
 
-// request.open("GET", "https://restcountries.com/v3.1/name/turkey");
-// request.send();
-
-// function setCousntry(data) {
-//   console.log(data.name.common);
-//   console.log("Ülkenin nüfusu " + data.population);
-//   console.log("Ülkenin başkenti " + data.capital);
-//   console.log("Ülkenin bölgesi " + data.subregion);
-//   console.log("Ülkenin parabirimi " + Object.values(data.currencies)[0].name);
-//   console.log(data.flags.svg); //flag img
-// }
-
 const setCountry = (country) => {
-  const request = new XMLHttpRequest();
-  request.open("GET", "https://restcountries.com/v3.1/name/" + country);
-  request.send();
-  request.addEventListener("load", () => {
-    const data = JSON.parse(request.responseText);
-    flagImg = data[0].flags.svg;
-    cardTitle = data[0].name.common;
-    displayCountry(data);
-  });
+  fetch("https://restcountries.com/v3.1/name/" + country)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          "Country not found. Please write the country name in English."
+        );
+      }
+      return response.json();
+    })
+    .then((data) => displayCountry(data))
+    .catch((error) => displayErr(error));
 };
 const displayCountry = (data) => {
   contentCount += 1;
+  let crn = Object.keys(data[0].currencies)[0];
   let mans = (data[0].population / 10000000).toFixed(0);
   let icons = `<i class="fa-solid fa-user"></i>`;
   let totalIcons = icons.repeat(mans);
   let countryCard = `
   <div class="card h-100" style="width: 18rem">
         <div class="card-header">
-          <h5 class="card-title">${cardTitle}</h5>
+          <h5 class="card-title">${data[0].name.common}</h5>
         </div>
-        <img src="${flagImg}" class="card-img-top" />
+        <img src="${data[0].flags.svg}" class="card-img-top" />
         <div class="card-body">
           <p class="card-text" id="card-text">
           The capital city of ${data[0].name.common} is ${data[0].capital}
@@ -52,13 +43,14 @@ const displayCountry = (data) => {
           ).toFixed(2)} Million</li>
           <li class="list-group-item"><b>Region :</b> ${data[0].subregion}</li>
           <li class="list-group-item"><b>Currency :</b> ${
-            Object.values(data[0].currencies)[0].name
-          }</li>
+            Object.values(Object.values(data[0].currencies)[0])[0]
+          } ${crn}</li>
         </ul>
         
       </div>
   `;
   let popMan = `${totalIcons}`;
+
   if (contentCount == 1) {
     content.innerHTML = countryCard;
     countryInfo1.innerHTML = popMan;
@@ -71,7 +63,9 @@ const displayCountry = (data) => {
     countryInfo1.innerHTML = popMan;
   }
 };
-
+const displayErr = (error) => {
+  return alert(error.message);
+};
 btn1.addEventListener("click", () => {
   country = input1.value;
   setCountry(country);
